5f39ba134f03f67ffc53352012d7ed5145a9bd29
SirSnow <hakuhal.snow@gmail.com>
2020-08-18 20:16:39 +0800

    因为之前使用的createjs的preload不能够使用了，经过考虑使用preload-it经过测试可以正常运行

---
+++ src/js/engine/preload.js
@@ -0,0 +1,28 @@
+import Preload from "preload-it";
+import { RESOURCE_TO_PRELOAD } from "./constants";
+const preloader = Preload();
+
+function preloadFunc() {
+  this.fetch(RESOURCE_TO_PRELOAD).then((items) => {
+    // use either a promise or 'oncomplete'
+    console.log(items);
+  });
+
+  this.oncomplete = (items) => {
+    console.log(items);
+  };
+
+  this.onprogress = (event) => {
+    console.log(event.progress + "%");
+  };
+
+  this.onfetched = (item) => {
+    console.log(item);
+  };
+
+  this.onerror = (item) => {
+    console.log(item);
+  };
+}
+
+export const preloadHandler = preloadFunc.bind(preloader);

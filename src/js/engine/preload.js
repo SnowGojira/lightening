6a663d8419376562be07df707cdca97f58668441
SirSnow <hakuhal.snow@gmail.com>
2020-08-18 21:46:57 +0800

    停到了98%就不走了，调试一下bug

--- src/js/engine/preload.js
+++ src/js/engine/preload.js
@@ -1,17 +1,22 @@
 import Preload from "preload-it";
 import { RESOURCE_TO_PRELOAD } from "./constants";
-import { prelaodAnimation, preloadAnimation } from "../ui/ui";
+import { preloadAnimation, onCompleteHandler } from "../ui/ui";
 const preloader = Preload();
 
 function preloadFunc() {
   this.fetch(RESOURCE_TO_PRELOAD);
 
   this.oncomplete = (items) => {
-    console.log(items);
+    // console.log(items);
+    onCompleteHandler();
   };
 
   this.onprogress = (event) => {
     preloadAnimation(event.progress);
+  };
+
+  this.onfetched = (item) => {
+    // console.log(item);
   };
 
   this.onerror = (item) => {
--- src/js/ui/ui.js
+++ src/js/ui/ui.js
@@ -4,3 +4,14 @@ export const preloadAnimation = (percentage) => {
   $("#loadPercent").text(percentage + "%");
   $(".pl_head").css("opacity", percentage / 100);
 };
+
+export const onCompleteHandler = () => {
+  $("#loadPercent").text("100%");
+  setTimeout(function () {
+    $(".pageTest").animate({ opacity: "0" }, 800, function () {
+      $(".pageTest").hide();
+      $(".pageLoad").show();
+      $(".pageLoad").animate({ opacity: "1.0" }, 800);
+    });
+  }, 2000);
+};

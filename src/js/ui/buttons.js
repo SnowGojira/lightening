7ff58049cd2bfa69617c46aabbd13ab57e31dfbb
SirSnow <hakuhal.snow@gmail.com>
2020-08-19 21:59:31 +0800

    分析了一下button的逻辑，晕的是，好像真的不用gameState，重点都在UI逻辑上面

--- src/js/ui/buttons.js
+++ src/js/ui/buttons.js
@@ -2,5 +2,18 @@ const buttons = document.querySelector(".q_optionContainer");
 
 export default function initButtons() {
   //点击
-  buttons.addEventListener("click", function () {});
+  buttons.addEventListener("click", function () {
+    //有UI逻辑
+    hint.hide();
+    //变色反馈
+    var id_str = $(this).attr("data-opt");
+    console.log("id:" + id_str);
+    $("#" + id_str).show();
+    //有计数逻辑
+    count += parseInt($(this).attr("data-value"));
+    console.log("count:" + count);
+
+    //音效
+    audioPlay("btn");
+  });
 }
